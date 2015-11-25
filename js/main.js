a622b8a1275603633e3105f6fd0fbfe9c0b1c8c6
jonygli <jonygli@users.noreply.github.com>
2015-11-25 18:52:31 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,6 +71,12 @@
 
     // render proj
     var list = [{
+        url: 'http://tencentopen.github.io/behaviac/',
+        name: 'behaviac',
+        img: 'img/proj/behaviac.png',
+        brief: '游戏AI的开发框架组件',
+        intro: 'behaviac是游戏AI的开发框架组件，也是游戏原型的快速设计工具。支持行为树BT，状态机FSM，HTN等多种范式，方便的编辑和调试。支持全平台，适用于客户端和服务器，助力游戏快速迭代开发。'
+    }, {
         url: 'http://alloyteam.github.io/JX/',
         name: 'JX',
         img: 'img/proj/jx.png',
